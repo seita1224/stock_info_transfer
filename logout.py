f1e84293b580452942b258c1cd9115b853dd1beb
YukiOkamoto <acro.okamoto@YukiOkamotonoMacBook-Pro.local>
2020-10-11 17:15:43 +0900

    ログ設定処理を変更

--- logout.py
+++ logout.py
@@ -8,30 +8,22 @@ from logging import getLogger
 import os
 
 
+file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'log', 'stock_info_transfer.log')
+logging.basicConfig(level=logging.DEBUG, filename=file_name, format="%(asctime)s %(levelname)8s %(message)s")
 logger = getLogger('stock_info_transfer')
-# handler = logging.FileHandler('./log/stock_info_transfer.log')
-
-file_handler_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'log', 'stock_info_transfer.log')
-handler = logging.FileHandler(file_handler_path)
-handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
-logger.addHandler(handler)
 
 
 def output_log_info(class_obj=None, log_message='EmptyMessage'):
-    logger.setLevel(logging.INFO)
     logger.info(class_obj.__class__.__name__+ ':' + log_message)
 
 
 def output_log_debug(class_obj=None, log_message='EmptyMessage'):
-    logger.setLevel(logging.DEBUG)
     logger.debug(class_obj.__class__.__name__+ ':' + log_message)
 
 
 def output_log_warning(class_obj=None, log_message='EmptyMessage'):
-    logger.setLevel(logging.WARNING)
     logger.warning(class_obj.__class__.__name__+ ':' + log_message)
 
 
 def output_log_error(class_obj=None, log_message='EmptyMessage'):
-    logger.setLevel(logging.ERROR)
     logger.error(class_obj.__class__.__name__+ ':' + log_message)
